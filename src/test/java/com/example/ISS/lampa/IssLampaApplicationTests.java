66476f1e5a36601531c2f5d4caef2aaeb571b7aa
77satyr77 <135996721+77satyr77@users.noreply.github.com>
2024-04-23 20:22:52 +0200

    Add files via upload

---
+++ src/test/java/com/example/ISS/lampa/IssLampaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ISS.lampa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssLampaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
